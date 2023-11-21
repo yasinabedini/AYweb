18f82a08d5d12de33833dd3db11573eb80ca4e44
yasin abedini <yasinabedini1384@gmail.com>
2023-11-21 17:26:16 +0330

    feat - add Blog Index & Blog-Details

---
+++ AYweb.Web/Controllers/BlogController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AYweb.Web.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult BlogDetails(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ AYweb.Web/Migrations/20231118203923__Mig_UpdateComment.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AYweb.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class _Mig_UpdateComment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreateDate",
+                table: "Comment",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreateDate",
+                table: "Comment");
+        }
+    }
+}
