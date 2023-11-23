68ec65783de3e390bb321a9e7c449853dfc46a61
yasin abedini <yasinabedini1384@gmail.com>
2023-11-23 20:59:03 +0330

    feat - configure Show All Blogs And Blog Details

---
+++ AYweb.Core/DTOs/BlogViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AYweb.Core.DTOs;
+
+public class ShowBlogViewModel
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string Summery { get; set; }
+    public string ImageName { get; set; }
+    public string UserName { get; set; }
+    public DateTime CreateDate { get; set; }
+
+}
--- AYweb.Web/Controllers/BlogController.cs
+++ AYweb.Web/Controllers/BlogController.cs
@@ -1,17 +1,60 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AYweb.Core.Services.Interfaces;
+using AYweb.Dal.Entities.Product;
+using AYweb.Dal.Entities.Service;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AYweb.Web.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
+        private readonly IBlogService _service;
+
+        public BlogController(IBlogService service)
         {
-            return View();
+            _service = service;
+        }
+
+        public IActionResult Index(int pageId = 1, string search = "")
+        {
+            int take = 8;
+
+            var newsList = _service.GetAllNews(pageId, search, take);
+
+            ViewBag.pageId = pageId;
+
+            if (pageId > 1)
+            {
+                ViewBag.lastPage = pageId - 1;
+            }
+            else
+            {
+                ViewBag.lastPage = 1;
+            }
+
+            if (pageId == newsList.Item2 || pageId > newsList.Item2)
+            {
+                ViewBag.nextPage = pageId;
+            }
+            else
+            {
+                ViewBag.nextPage = pageId + 1;
+            }
+
+            ViewBag.take = take;
+            return View(newsList);
         }
 
         public IActionResult BlogDetails(int id)
         {
-            return View();
+            var news = _service.GetNewsById(id);
+            if (news==null)
+            {
+                return BadRequest();
+            }
+
+            ViewData["tags"] = _service.GetAllTags();
+
+            return View(news);
         }
     }
 }
