2535f3f798d045c842120ecd2f20ffcff6ef40c9
yasin abedini <yasinabedini1384@gmail.com>
2023-12-13 00:12:02 +0330

    feat - some Section From Home/Index now Are Dynamic

---
+++ AYweb.Web/ViewComponents/LastNewsViewComponents.cs
@@ -0,0 +1,21 @@
+﻿using AYweb.Core.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Microsoft.IdentityModel.Tokens;
+
+namespace AYweb.Web.ViewComponents;
+
+public class LastNewsViewComponents:ViewComponent
+{
+    private readonly IBlogService _service;
+
+    public LastNewsViewComponents(IBlogService service)
+    {
+        _service = service;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        return await Task.FromResult((IViewComponentResult)View("LastNews", _service.GetAllNews().Item1.Take(3).ToList()));
+    }
+}
