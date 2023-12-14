b1c35a587120edde556c67ff0ab70e1d3f858d48
yasin abedini <yasinabedini1384@gmail.com>
2023-12-14 20:03:38 +0330

    feat - Add NEw Admin Panel

---
+++ AYweb.Web/Pages/Admin/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AYweb.Web.Pages.Admin
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
