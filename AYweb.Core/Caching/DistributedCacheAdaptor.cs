618ae9b3aa5898606f0c694a496daebad1aa14a1
yasin abedini <yasinabedini1384@gmail.com>
2023-12-22 15:55:00 +0330

    feat - Add Cache Adaptor For Caching

---
+++ AYweb.Core/Caching/DistributedCacheAdaptor.cs
@@ -0,0 +1,46 @@
+﻿using AYweb.Core.Serializer;
+using Microsoft.Extensions.Caching.Distributed;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Core.Caching
+{
+    public class DistributedCacheAdaptor : ICacheAdaptor
+    {
+        private readonly IDistributedCache _cache;
+        private readonly IJsonSerializer _serializer;
+
+        public DistributedCacheAdaptor(IDistributedCache cache, IJsonSerializer serializer)
+        {
+            _cache = cache;
+            _serializer = serializer;
+        }
+
+        public void Add(string key, object value, int absoluteExpirationMinutes, int slidingExpirationMinutes)
+        {
+            string valueStr = _serializer.Serialize(value);
+            _cache.SetString(key, valueStr, new DistributedCacheEntryOptions
+            {
+                AbsoluteExpiration = DateTime.Now.AddMinutes(absoluteExpirationMinutes),
+                SlidingExpiration = TimeSpan.FromHours(slidingExpirationMinutes)
+            });
+        }
+
+        public T Get<T>(string key)
+        {
+            var valueStr = _cache.GetString(key) ?? "";
+            return _serializer.Deserialize<T>(valueStr);
+
+        }
+
+        public void Remove(string key)
+        {
+            _cache.Remove(key);
+        }
+
+     
+    }
+}
---
+++ AYweb.Core/Caching/ICacheAdaptor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Core.Caching
+{
+    public interface ICacheAdaptor
+    {
+        void Add(string key, object value, int absoluteExpirationMinutes, int slidingExpirationMinutes);
+        void Remove(string key);
+        T Get<T>(string key);
+    }
+}
