f82a0e9f44d0508cda8d6146f6fe80bba73031b3
yasin abedini <yasinabedini1384@gmail.com>
2023-12-30 23:39:06 +0330

    faet - add some snum for simple work

---
+++ AYweb.Dal/Enums/_OrderStatus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Dal.Enums
+{
+    public enum _OrderStatus
+    {
+        Cart=1,
+        AwaitingApproval=2,
+        Failed = 3,
+        PreparingSend = 4,
+        Posted =5
+                        
+    }
+}
---
+++ AYweb.Dal/Enums/_TransactionType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Dal.Enums
+{
+    public enum _TransactionType
+    {
+        PaymentPlan = 1,
+        PaymentOrder = 2,
+        PaymentService =3
+    }
+}
