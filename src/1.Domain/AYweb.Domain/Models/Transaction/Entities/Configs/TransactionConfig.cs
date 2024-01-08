57e32ee70ee5ba0f65667ee9e0a369ebfc965535
yasin abedini <yasinabedini1384@gmail.com>
2024-01-09 02:20:05 +0330

    feat - add config file

---
+++ src/1.Domain/AYweb.Domain/Models/Transaction/Entities/Configs/TransactionConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Transaction.Entities.Configs;
+
+public class TransactionConfig : IEntityTypeConfiguration<Transaction>
+{
+    public void Configure(EntityTypeBuilder<Transaction> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
