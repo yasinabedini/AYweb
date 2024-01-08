1187acfbcaa440b60d9ffeb4c018fd8cbed86ae9
yasin abedini <yasinabedini1384@gmail.com>
2024-01-09 02:20:17 +0330

    faet - add gallery aggregate

---
+++ src/1.Domain/AYweb.Domain/Models/Gallery/Entities/Configs/GalleryConfig.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Gallery.Entities.Configs;
+
+public class GalleryConfig : IEntityTypeConfiguration<Gallery>
+{
+    public void Configure(EntityTypeBuilder<Gallery> builder)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/1.Domain/AYweb.Domain/Models/Gallery/Entities/Gallery.cs
@@ -0,0 +1,44 @@
+﻿using AIPFramework.Entities;
+
+namespace AYweb.Domain.Models.Gallery.Entities;
+
+public class Gallery : AggregateRoot
+{
+    #region Properties
+    public string ImageName { get; private set; }
+
+    public bool IsDelete { get; set; }
+    #endregion
+
+    #region Constructors And Factories
+    private Gallery() { }
+    public Gallery(string imageName)
+    {
+        ImageName = imageName;
+    }
+    public static Gallery Create(string imageName)
+    {
+        return new Gallery(imageName);
+    }
+    #endregion
+
+    #region Command
+
+    private void Modified()
+    {
+        ModifiedAt = DateTime.Now;
+    }
+
+    public void Delete()
+    {
+        IsDelete = true;
+        Modified();
+    }
+
+    public void ChangeImageName(string imageName)
+    {
+        ImageName = imageName;
+        Modified();
+    }
+    #endregion
+}
