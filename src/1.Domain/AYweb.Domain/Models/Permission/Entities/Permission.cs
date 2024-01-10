b590057635e946123f3e8b09aa386d18d1b001e4
yasin abedini <yasinabedini1384@gmail.com>
2024-01-10 23:33:08 +0330

    feat - Add Permission Entity

---
+++ src/1.Domain/AYweb.Domain/Models/Permission/Entities/Permission.cs
@@ -0,0 +1,53 @@
+﻿using AIPFramework.Entities;
+using AYweb.Domain.Common.ValueObjects;
+using AYweb.Domain.Models.Role.Entities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AYweb.Domain.Models.Permission.Entities;
+
+public class Permission : AggregateRoot
+{
+    #region Properties
+    public Title Title { get; private set; }
+
+    public int? ParentId { get; private set; }
+
+    public Permission? Parent { get; private set; }
+
+    public bool IsDelete { get; private set; }
+    
+    #endregion
+
+    #region Constructor And Factories
+    private Permission() { }
+    public Permission(string title, int? parentId)
+    {
+        Title = new Title(title);
+        ParentId = parentId;
+        CreateAt = DateTime.Now;
+    }
+    public static Permission Create(string title, int? parentId)
+    {
+        return new Permission(title, parentId);
+    }
+    #endregion
+
+    #region Common
+    private void Modified()
+    {
+        ModifiedAt = DateTime.Now;
+    }
+
+    public void ChangeTitle(string title)
+    {
+        Title = new Title(title);
+    }
+
+    public void Delete()
+    {
+        IsDelete = true;
+        Modified();
+    }
+    #endregion
+}
