b9b975353fba16b927285b416c0dc969aff170f3
yasin abedini <yasinabedini1384@gmail.com>
2024-01-10 23:32:58 +0330

    feat - Add Role Entity

---
+++ src/1.Domain/AYweb.Domain/Models/Role/Entities/Role.cs
@@ -0,0 +1,60 @@
+﻿using AIPFramework.Entities;
+using AYweb.Domain.Common.ValueObjects;
+
+namespace AYweb.Domain.Models.Role.Entities
+{
+    public class Role : AggregateRoot
+    {
+        #region Properties
+        public Title Title { get;private set; }
+
+        public bool IsDelete { get; private set; }
+
+        public List<Role_Permission> Permissions { get; set; }
+        public List<Role_Users> Role_Users { get; set; }
+        #endregion
+
+        #region Constructor And Factories
+        private Role() { }
+        public Role(string title)
+        {
+            Title = new Title(title);
+            CreateAt = DateTime.Now;
+        }
+        public static Role Create(string title)
+        {
+            return new Role(title);
+        }
+        #endregion
+
+        #region Common
+
+        private void Modified()
+        {
+            ModifiedAt = DateTime.Now;
+        }
+
+        public void ChangeTitle(string title)
+        {
+            Title = new Title(title);
+            Modified();
+        }
+        
+        public void AddUserToRole(Role_Users user)
+        {
+            Role_Users.Add(user);            
+        }
+
+        public void AddPermissionToRole(Role_Permission role_Permission)
+        {
+            Permissions.Add(role_Permission);
+        }
+
+        public void Delete()
+        {
+            IsDelete = true;
+            Modified();
+        }
+        #endregion
+    }
+}
---
+++ src/1.Domain/AYweb.Domain/Models/Role/Entities/Role_Permission.cs
@@ -0,0 +1,45 @@
+﻿using AIPFramework.Entities;
+using AYweb.Domain.Models.Permission.Entities;
+
+namespace AYweb.Domain.Models.Role.Entities;
+public class Role_Permission : Entity
+{
+    #region Properties
+    public int RoleId { get; set; }
+
+    public int PermissionId { get; set; }
+
+    public bool IsDelete { get; set; }
+
+    public Role Role { get; set; }
+
+    public Permission.Entities.Permission Permission { get; set; }    
+    #endregion
+
+    #region Constructor And Factories
+    private Role_Permission() { }
+    public Role_Permission(int roleId, int permissionId)
+    {
+        RoleId = roleId;
+        PermissionId = permissionId;
+        CreateAt = DateTime.Now;
+    }
+    public static Role_Permission Create(int roleId, int permissionId)
+    {
+        return new Role_Permission(roleId, permissionId);
+    }
+    #endregion
+
+    #region Common
+    private void Modified()
+    {
+        ModifiedAt = DateTime.Now;
+    }
+
+    public void Delete()
+    {
+        IsDelete = true;
+        Modified();
+    } 
+    #endregion
+}
