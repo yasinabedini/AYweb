254ac8d49bb629ec8d8b88306395cea8c28efacf
yasin abedini <yasinabedini1384@gmail.com>
2024-01-10 23:32:29 +0330

    feat - Add Academy Entity

---
+++ src/1.Domain/AYweb.Domain/Models/Academy/Entities/Academy.cs
@@ -0,0 +1,44 @@
+﻿using AIPFramework.Entities;
+using System.ComponentModel.DataAnnotations;
+
+namespace AYweb.Domain.Models.Academy.Entities;
+
+public class Academy : Entity
+{        
+    public string Name { get;private set; }
+   
+    public int TeamCount { get; private set; }
+        
+    public int ProjectCount { get; private set; }
+
+    private Academy(string name,int teamCount,int projectCount)
+    {
+        Name = name;
+        TeamCount = teamCount;
+        ProjectCount = projectCount;
+        CreateAt = DateTime.Now;
+    }
+
+    public void Modified()
+    {
+        ModifiedAt = DateTime.Now;
+    }
+
+    public void ChangeName(string name)
+    {
+        Name = name;
+        Modified();
+    }
+
+    public void ChangeTeamCount(int teamCount)
+    {
+        TeamCount = teamCount;
+        Modified();
+    }
+
+    public void ChangeProjectCount(int projectCount)
+    {
+        ProjectCount=projectCount;
+        Modified();
+    }
+}
---
+++ src/1.Domain/AYweb.Domain/Models/Academy/Entities/Configs/AcademyConfig.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Academy.Entities.Configs
+{
+    public class AcademyConfig : IEntityTypeConfiguration<Academy>
+    {
+        public void Configure(EntityTypeBuilder<Academy> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
