d1ef8d921f87c5745271f6d3a0f5b2e5a81496a6
yasin abedini <yasinabedini1384@gmail.com>
2024-01-11 23:29:40 +0330

    feat - Add Main Repository Contract

---
+++ src/1.Domain/AYweb.Domain/Common/Repositories/IRepository.cs
@@ -0,0 +1,19 @@
+﻿using AIPFramework.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Common.Repositories
+{
+    public interface IRepository<TEntity>
+    {
+        TEntity GetById(long id);
+        void Add(TEntity entity);
+        List<TEntity> GetList();
+        void Update(TEntity entity);
+        void Save();
+
+    }
+}
