265163dbd7715781efb76ecf4399ce2b35b7dc3a
yasin abedini <yasinabedini1384@gmail.com>
2024-01-14 23:35:05 +0330

    feat - Config Base Repository

---
+++ src/2.Infrastructure/AYweb.Infrastructure/Common/Repository/BaseRepository.cs
@@ -0,0 +1,47 @@
+﻿using AIPFramework.Entities;
+using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.User.Entities;
+using AYweb.Infrastructure.Contexts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Infrastructure.Common.Repository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : AggregateRoot
+    {
+        private readonly AyWebDbContext _context;
+
+        public BaseRepository(AyWebDbContext context)
+        {
+            _context = context;
+        }
+
+        public void Add(TEntity entity)
+        {
+            _context.Set<TEntity>().Add(entity);
+        }
+
+        public TEntity GetById(long id)
+        {
+            return _context.Set<TEntity>().Find(id);
+        }
+
+        public List<TEntity> GetList()
+        {
+            return _context.Set<TEntity>().ToList();
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void Update(TEntity entity)
+        {
+            _context.Set<TEntity>().Update(entity);
+        }
+    }
+}
