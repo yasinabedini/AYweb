5c18f877cde321eebdf805f49cc615732ef4b9bf
yasin abedini <yasinabedini1384@gmail.com>
2024-01-18 19:49:17 +0330

    feat - Add Comment Repository

---
+++ src/1.Domain/AYweb.Domain/Models/Product/Repositories/ICommentRepository.cs
@@ -0,0 +1,14 @@
+﻿using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.Product.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Product.Repositories
+{
+    public interface ICommentRepository:IRepository<Comment>
+    {
+    }
+}
