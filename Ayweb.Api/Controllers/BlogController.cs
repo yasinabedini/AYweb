79436930ab4774e6e9ab6143fde252e627eaca9b
yasin abedini <yasinabedini1384@gmail.com>
2024-01-19 18:22:11 +0330

    fix - fix some bugs

---
+++ Ayweb.Api/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿using AYweb.Application.Models.Blog.Queries.GetBlogs;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ayweb.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public BlogController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll(int pageId = 1)
+        {
+            return Ok(_sender.Send(new GetBlogsQuery { PageNumber = pageId, PageSize = 12 }).Result.QueryResult);
+        }
+    }
+}
