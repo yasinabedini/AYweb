72a07c67a567f3917144517fb5fccc6f73b3d817
yasin abedini <yasinabedini1384@gmail.com>
2024-01-19 18:20:20 +0330

    feat - Add Blogs Repositories

---
+++ src/1.Domain/AYweb.Domain/Models/Blog/Repositories/IBlogCommentRepository.cs
@@ -0,0 +1,10 @@
+﻿using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.Blog.Entities;
+
+namespace AYweb.Domain.Models.Blog.Repositories
+{
+    public interface IBlogCommentRepository:IRepository<BlogComment>
+    {
+        List<BlogComment> GetBlogComments(long blogId);
+    }
+}
---
+++ src/1.Domain/AYweb.Domain/Models/Blog/Repositories/IBlogGroupRepository.cs
@@ -0,0 +1,14 @@
+﻿using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.Blog.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Blog.Repositories
+{
+    public interface IBlogGroupRepository:IRepository<BlogGroup>
+    {
+    }
+}
