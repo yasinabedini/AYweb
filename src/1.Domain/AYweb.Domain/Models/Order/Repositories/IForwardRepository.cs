d008b7c0b48dee50885d164f53db6b85b8b3227d
yasin abedini <yasinabedini1384@gmail.com>
2024-01-19 23:33:08 +0330

    feat - Add Repository related Order

---
+++ src/1.Domain/AYweb.Domain/Models/Order/Repositories/IForwardRepository.cs
@@ -0,0 +1,14 @@
+﻿using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.Order.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Order.Repositories
+{
+    public interface IForwardRepository:IRepository<Forward>
+    {
+    }
+}
---
+++ src/1.Domain/AYweb.Domain/Models/Order/Repositories/IOrderLineRepository.cs
@@ -0,0 +1,15 @@
+﻿using AYweb.Domain.Common.Repositories;
+using AYweb.Domain.Models.Order.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Domain.Models.Order.Repositories
+{
+    public interface IOrderLineRepository:IRepository<OrderLine>
+    {
+        List<OrderLine> GetOrderOrderLines(long orderId);
+    }
+}
