e5cba3d66d922d52e241f71d9d2b496abfd8779b
yasin abedini <yasinabedini1384@gmail.com>
2024-01-19 23:33:45 +0330

    feat - Add Some User Queries

---
+++ src/3.Application/AYweb.Application/Models/User/Queries/GetAuthenticatedUser/GetAuthenticatedUserQuery.cs
@@ -0,0 +1,14 @@
+﻿using AIPFramework.Commands;
+using AIPFramework.Queries;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Queries.GetAuthenticatedUser
+{
+    public class GetAuthenticatedUserQuery:IQuery<Domain.Models.User.Entities.User>
+    {
+    }
+}
---
+++ src/3.Application/AYweb.Application/Models/User/Queries/GetAuthenticatedUser/GetAuthenticatedUserQueryHandler.cs
@@ -0,0 +1,25 @@
+﻿using AIPFramework.Queries;
+using AYweb.Domain.Models.User.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Queries.GetAuthenticatedUser
+{
+    public class GetAuthenticatedUserQueryHandler : IQueryHandler<GetAuthenticatedUserQuery, Domain.Models.User.Entities.User>
+    {
+        private readonly IUserRepository _repository;
+
+        public GetAuthenticatedUserQueryHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<Domain.Models.User.Entities.User> Handle(GetAuthenticatedUserQuery request, CancellationToken cancellationToken)
+        {
+            return Task.FromResult(_repository.GetAuthenticatedUser());
+        }
+    }
+}
