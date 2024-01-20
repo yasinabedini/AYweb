45a749c6373f1008075f103bd775eb8743cfe8b7
yasin abedini <yasinabedini1384@gmail.com>
2024-01-20 11:14:34 +0330

    Feat - Add SHow Products And Product

---
+++ src/4.Presentation/AYweb.Presentation/Controllers/ProductController.cs
@@ -0,0 +1,51 @@
+﻿using AYweb.Application.Models.Product.Queries.GetProduct;
+using AYweb.Application.Models.Product.Queries.GetProducts;
+using AYweb.Domain.Models.Service.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYweb.Presentation.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly ISender _sender;
+
+        public ProductController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public IActionResult Index(int pageId = 1, string filter = "", string orderBy = "")
+        {
+            int take = 1;
+            var products = _sender.Send(new GetProductsQuery { PageNumber = pageId, PageSize = take });
+            
+            if (pageId > 1)
+            {
+                ViewBag.lastPage = pageId - 1;
+            }
+            else
+            {
+                ViewBag.lastPage = 1;
+            }
+
+            if (pageId == products.Result.pageCount || pageId > products.Result.pageCount)
+            {
+                ViewBag.nextPage = pageId;
+            }
+            else
+            {
+                ViewBag.nextPage = pageId + 1;
+            }            
+
+            return View(products.Result);
+        }
+
+        [HttpGet]
+        [Route("Product/{id}")]
+        public IActionResult ProductDetails(int id)
+        {
+            return View(_sender.Send(new GetProductQuery { Id =id}).Result);
+        }
+    }
+}
