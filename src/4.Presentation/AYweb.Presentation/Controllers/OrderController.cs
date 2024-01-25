5733f23e66e9670427b2cc8c926aa6aaa29cbf4a
yasin abedini <yasinabedini1384@gmail.com>
2024-01-26 01:24:51 +0330

    feat - Add MyCart Controller And View

---
+++ src/4.Presentation/AYweb.Presentation/Controllers/OrderController.cs
@@ -0,0 +1,36 @@
+﻿using AYweb.Application.Models.Order.Queries.GetCurrentUserCurrentOrder;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYweb.Web.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly ISender _sender;
+
+        public OrderController(ISender sender)
+        {
+            _sender = sender;
+        }
+     
+
+        public IActionResult MyCart()
+        {
+            return View(_sender.Send(new GetCurrentUserCurrentOrderQuery()).Result);
+        }
+
+        [HttpPost]
+        public void ChangeOrderLineCount(int productId, int count)
+        {
+          //  _service.ChangeOrderLineCount(productId, count, HttpContext);
+        }
+
+        public IActionResult DeleteOrderLine(int productId)
+        {
+            //_service.DeleteOrderLine(productId, HttpContext);
+            return RedirectToAction("MyCart", "Index", new { area = "UserPanel" });
+        }
+
+
+    }
+}
