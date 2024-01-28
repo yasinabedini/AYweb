1608ae459381a6a94a1e499d9ffba5718778da2e
yasin abedini <yasinabedini1384@gmail.com>
2024-01-28 12:10:48 +0330

    feat - Add Eror View

---
+++ src/4.Presentation/AYweb.Presentation/Controllers/ErrorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AYweb.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
