2fdc0bec4d8555dd8db21425f09bac2f8236ee03
yasin abedini <yasinabedini1384@gmail.com>
2024-01-29 12:06:24 +0330

    fix - fix bug from Session

--- src/4.Presentation/AYweb.Presentation/DependencyInjection.cs
+++ src/4.Presentation/AYweb.Presentation/DependencyInjection.cs
@@ -9,38 +9,55 @@ namespace AYweb.Presentation
     {
         public static IServiceCollection AddPresentation(this IServiceCollection services)
         {
+
+            #region DbContext
             services.AddDbContext<AyWebDbContext>(option => option.UseSqlServer("server=YasiAbdn\\ABDN;initial catalog=Db-AyWeb2;integrated Security=true;TrustServerCertificate=True"));
+            #endregion
 
             services.AddHttpContextAccessor();
 
+            #region Razor And Views
             services.AddControllersWithViews();
-            services.AddRazorPages();
+
+            services.AddRazorPages(); 
+            #endregion
+
+            #region Session
+            services.AddTransient<ISessionAdaptor, SessionAdaptor>();
 
             services.AddDistributedMemoryCache();
-           
+
+            services.AddSession(options =>
+            {
+                options.IdleTimeout = TimeSpan.FromDays(15);
+                options.Cookie.HttpOnly = true;
+                options.Cookie.IsEssential = true;
+            });
+            #endregion
+
+            #region Authentication And Cookie
+
             services.AddAuthentication();
 
-            services.AddTransient<ISessionAdaptor, SessionAdaptor>();
-            services.AddSession(t =>
-            {
-                t.IOTimeout = TimeSpan.FromDays(15);
-                t.IdleTimeout = TimeSpan.FromDays(15);                                
-            });
-
-
             services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-                options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
-            }).AddCookie(options =>
-            {
-                options.LoginPath = "/Login";
-                options.LogoutPath = "/LogOut";
-                options.SlidingExpiration = true;
-                options.ExpireTimeSpan = TimeSpan.FromMinutes(43200);
-
-            });
+                {
+                    options.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                    options.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                }).AddCookie(options =>
+                {
+                    options.LoginPath = "/Login";
+                    options.LogoutPath = "/LogOut";
+                    options.SlidingExpiration = true;
+                    options.ExpireTimeSpan = TimeSpan.FromMinutes(43200);
+                    options.Cookie = new CookieBuilder()
+                    {
+                        SecurePolicy = CookieSecurePolicy.Always,
+                        HttpOnly = true,
+                        SameSite = SameSiteMode.None
+                    };
+                });
+            #endregion
 
             return services;
         }
