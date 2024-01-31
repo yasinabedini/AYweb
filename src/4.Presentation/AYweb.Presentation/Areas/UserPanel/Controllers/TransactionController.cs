ef45e524c9e5dd4c44dc18fb80e79867758b8037
yasin abedini <yasinabedini1384@gmail.com>
2024-01-31 19:29:09 +0330

    feat - Add Transaction Index Controller And View

---
+++ src/4.Presentation/AYweb.Presentation/Areas/UserPanel/Controllers/TransactionController.cs
@@ -0,0 +1,23 @@
+﻿using AYweb.Application.Models.Transaction.Queries.GetCurrentUserTransactions;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AYweb.Presentation.Areas.UserPanel.Controllers
+{
+    [Area("UserPanel")]
+    public class TransactionController : Controller
+    {
+        private readonly ISender _sender;
+
+        public TransactionController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [Route("Transactions")]
+        public IActionResult Index()
+        {
+            return View(_sender.Send(new GetCurrentUserTransactionsQuery()).Result);
+        }
+    }
+}
