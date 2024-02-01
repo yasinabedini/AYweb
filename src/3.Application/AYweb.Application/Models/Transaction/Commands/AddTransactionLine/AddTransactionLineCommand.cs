ffe84a20e2d3a9dc7677d929e3faa52e2c91db93
yasin abedini <yasinabedini1384@gmail.com>
2024-02-01 23:47:11 +0330

    feat - Add TransactionLine Command

---
+++ src/3.Application/AYweb.Application/Models/Transaction/Commands/AddTransactionLine/AddTransactionLineCommand.cs
@@ -0,0 +1,17 @@
+﻿using AIPFramework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.Transaction.Commands.AddTransactionLine
+{   
+    public class AddTransactionLineCommand:ICommand
+    {
+        public required long TransactionId { get;  set; }
+        public required string Commodity { get;  set; }
+        public required int Amount { get;  set; }
+        public required int UnitPrice { get;  set; }
+    }
+}
---
+++ src/3.Application/AYweb.Application/Models/Transaction/Commands/AddTransactionLine/AddTransactionLineCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using AIPFramework.Commands;
+using AYweb.Domain.Models.Transaction.Entities;
+using AYweb.Domain.Models.Transaction.Repositories;
+
+namespace AYweb.Application.Models.Transaction.Commands.AddTransactionLine
+{
+    public class AddTransactionLineCommandHandler : ICommandHandler<AddTransactionLineCommand>
+    {
+        private readonly ITransactionLineRepository _repository;
+
+        public AddTransactionLineCommandHandler(ITransactionLineRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(AddTransactionLineCommand request, CancellationToken cancellationToken)
+        {
+            _repository.Add(TransactionLine.Create(request.TransactionId, request.Commodity, request.Amount, request.UnitPrice));
+            _repository.Save();
+            
+            return Task.CompletedTask;
+        }
+    }
+}
