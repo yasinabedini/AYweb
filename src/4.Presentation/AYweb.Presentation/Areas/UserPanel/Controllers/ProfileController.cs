385a6e58cf69e0874f8147b0487d331def2d174d
yasin abedini <yasinabedini1384@gmail.com>
2024-02-04 12:32:28 +0330

    fix - fix some bugs

--- src/4.Presentation/AYweb.Presentation/Areas/UserPanel/Controllers/ProfileController.cs
+++ src/4.Presentation/AYweb.Presentation/Areas/UserPanel/Controllers/ProfileController.cs
@@ -1,8 +1,12 @@
-﻿using AYweb.Application.Models.Plan.Commands.AddPlanToUser;
+﻿using AYweb.Application.Models.Order.Commands.ChnageOrderLineAmount;
+using AYweb.Application.Models.Plan.Commands.AddPlanToUser;
 using AYweb.Application.Models.Plan.Queries.GetPlan;
 using AYweb.Application.Models.Transaction.Commands.AddTransactionLine;
 using AYweb.Application.Models.Transaction.Commands.CreateTransaction;
+using AYweb.Application.Models.User.Commands.ChangePassword;
+using AYweb.Application.Models.User.Commands.UpdateUserInformation;
 using AYweb.Application.Models.User.Queries.GetAuthenticatedUser;
+using AYweb.Application.Security;
 using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -39,6 +43,70 @@ namespace AYweb.Presentation.Areas.UserPanel.Controllers
             _sender.Send(new AddPlanToUserCommand { PlanId = plan.Id, UserId = user.Id, TransactionId = transactionId });
 
             return RedirectToAction("CheckOut", "Transaction", new { area = "UserPanel", id = transactionId });
+        }
+
+        [Route("Profile/ChangePassword")]
+        [HttpGet]
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [Route("Profile/ChangePassword")]
+        [HttpPost]
+        public IActionResult ChangePassword(ChangePasswordCommand changePassword)
+        {
+            var user = _sender.Send(new GetAuthenticatedUserQuery()).Result;
+            changePassword.Id = user.Id;
+
+            if (user.Password != PasswordHelper.EncodePasswordMd5(changePassword.OldPassword))
+            {
+                ViewData["WrongOldNotification"] = true;
+                return View();
+            }
+
+            if (changePassword.Password != changePassword.Re_Password)
+            {
+                ViewData["WrongRepeatNotification"] = true;
+                return View();
+            }
+
+            _sender.Send(changePassword);
+
+            ViewData["ChangeSuccess"] = true;
+            return View();
+        }
+
+        [Route("Profile/ChangeInfo")]
+        [HttpGet]
+        public IActionResult ChangeInfo()
+        {
+            var user = _sender.Send(new GetAuthenticatedUserQuery()).Result;
+            ViewData["User"] = user;
+
+            return View();
+        }
+
+        [Route("Profile/ChangeInfo")]
+        [HttpPost]
+        public IActionResult ChangeInfo(UpdateUserInformationCommmand updateUser)
+        {
+            var user = _sender.Send(new GetAuthenticatedUserQuery()).Result;
+            updateUser.Id = user.Id;
+
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            _sender.Send(updateUser);
+
+            ViewData["ChangeSuccess"] = true;
+
+            user = _sender.Send(new GetAuthenticatedUserQuery()).Result;
+            ViewData["User"] = user;
+
+            return View();
         }
     }
 }
