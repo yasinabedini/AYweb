a70bf15601cea37be7e70d9b20e9ae4988b9ecd2
yasin abedini <yasinabedini1384@gmail.com>
2024-02-04 23:44:55 +0330

    feat - Add Get New Password OPtion

---
+++ src/3.Application/AYweb.Application/Models/User/Commands/GetNewPassword/GetNewPasswordCommand.cs
@@ -0,0 +1,14 @@
+﻿using AIPFramework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Commands.GetNewPassword
+{
+    public class GetNewPasswordCommand:ICommand
+    {
+        public required string PhoneNumber { get; set; }
+    }
+}
---
+++ src/3.Application/AYweb.Application/Models/User/Commands/GetNewPassword/GetNewPasswordCommandHandler.cs
@@ -0,0 +1,39 @@
+﻿using AIPFramework.Commands;
+using AYweb.Application.Generators;
+using AYweb.Application.Security;
+using AYweb.Application.Senders;
+using AYweb.Domain.Models.User.Repositories;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Commands.GetNewPassword
+{
+    public class GetNewPasswordCommandHandler : ICommandHandler<GetNewPasswordCommand>
+    {
+        private readonly IUserRepository _repository;
+        private ISender _sender;
+
+        public GetNewPasswordCommandHandler(IUserRepository repository, ISender sender)
+        {
+            _repository = repository;
+            _sender = sender;
+        }
+
+        public Task Handle(GetNewPasswordCommand request, CancellationToken cancellationToken)
+        {
+            var user = _repository.GetUSerByPhoneNumber(request.PhoneNumber);
+            var newPassword = Generator.GenerateDefaultPassword();
+            var newHashPass = PasswordHelper.EncodePasswordMd5(newPassword);
+            user.ChangePassword(newHashPass);
+            _repository.Update(user);
+            _repository.Save();
+            Sms.SendNewPassword(user.PhoneNumber.Value, newPassword);
+
+            return Task.CompletedTask;
+        }
+    }
+}
