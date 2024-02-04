a417ddb6ebaa705851f4f118f276ad13e43fd9ba
yasin abedini <yasinabedini1384@gmail.com>
2024-02-04 12:32:03 +0330

    feat - Add Change user info View  And Action

---
+++ src/3.Application/AYweb.Application/Models/User/Commands/UpdateUserInformation/UpdateUserInformationCommmand.cs
@@ -0,0 +1,17 @@
+﻿using AIPFramework.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Commands.UpdateUserInformation
+{
+    public class UpdateUserInformationCommmand:ICommand
+    {
+        public long Id { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public string? Email { get; set; }
+    }
+}
---
+++ src/3.Application/AYweb.Application/Models/User/Commands/UpdateUserInformation/UpdateUserInformationCommmandHandler.cs
@@ -0,0 +1,30 @@
+﻿using AIPFramework.Commands;
+using AYweb.Domain.Models.User.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Commands.UpdateUserInformation
+{
+    public class UpdateUserInformationCommmandHandler : ICommandHandler<UpdateUserInformationCommmand>
+    {
+        private readonly IUserRepository _repository;
+
+        public UpdateUserInformationCommmandHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(UpdateUserInformationCommmand request, CancellationToken cancellationToken)
+        {
+            _repository.ChangeFirstName(request.Id,request.FirstName);
+            _repository.ChangeLastName(request.Id,request.LastName);
+            _repository.ChangeEmail(request.Id, request.Email??"");
+            _repository.Save();
+            
+            return Task.CompletedTask;
+        }
+    }
+}
