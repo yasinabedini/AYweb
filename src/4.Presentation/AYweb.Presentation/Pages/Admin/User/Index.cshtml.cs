012905f03d5d9e6523d1462ec7bdf68067e90c69
yasin abedini <yasinabedini1384@gmail.com>
2024-02-06 23:26:38 +0330

    feat - add user list

---
+++ src/4.Presentation/AYweb.Presentation/Pages/Admin/User/Index.cshtml.cs
@@ -0,0 +1,26 @@
+using AIPFramework.Queries;
+using AYweb.Application.Models.User.Queries.Common;
+using AYweb.Application.Models.User.Queries.GetUsers;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AYweb.Presentation.Pages.Admin.User
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ISender _sender;
+
+        public PagedData<UserResult> Users { get; set; }
+
+        public IndexModel(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public void OnGet(string search = "", int page = 1)
+        {
+            Users = _sender.Send(new GetUsersQuery() { Search = search, PageNumber = page, PageSize = 50 }).Result;
+        }
+    }
+}
