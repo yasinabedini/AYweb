c98362c99b878024f841b22dc13a4462e1b7a3e9
yasin abedini <yasinabedini1384@gmail.com>
2024-02-06 23:26:47 +0330

    feat - add blog list

---
+++ src/4.Presentation/AYweb.Presentation/Pages/Admin/Blog/Index.cshtml.cs
@@ -0,0 +1,27 @@
+using AIPFramework.Queries;
+using AYweb.Application.Models.Blog.Queries.Common;
+using AYweb.Application.Models.Blog.Queries.GetBlogs;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
+
+namespace AYweb.Presentation.Pages.Admin.Blog
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ISender _sender;
+
+        public PagedData<BlogResult> Blogs { get; set; }
+
+        public IndexModel(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public void OnGet(string search = "",int page = 1)
+        {
+            Blogs = _sender.Send(new GetBlogsQuery() {search = search,PageNumber = page,PageSize = 50}).Result;
+        }
+    }
+}
