bf541ac8f84a5e02ac8cfa421db9be7b929509e3
yasin abedini <yasinabedini1384@gmail.com>
2024-02-07 11:56:17 +0330

    fix -fix some bugs

--- src/3.Application/AYweb.Application/Models/User/Commands/CreateUser/CreateUserCommand.cs
+++ src/3.Application/AYweb.Application/Models/User/Commands/CreateUser/CreateUserCommand.cs
@@ -2,11 +2,12 @@
 
 namespace AYweb.Application.Models.User.Commands.CreateUser
 {
-    public class CreateUserCommand : ICommand<bool>
+    public class CreateUserCommand : ICommand<long>
     {
         public required string FirstName { get; set; }
         public  required string LastName { get; set; }
         public required string PhoneNumber { get; set; }
-        public required string Password { get; set; }      
+        public required string Password { get; set; }
+        public string?  Email { get; set; }
     }
 }
--- src/3.Application/AYweb.Application/Models/User/Commands/CreateUser/CreateUserCommandHandler.cs
+++ src/3.Application/AYweb.Application/Models/User/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -6,7 +6,7 @@ using Zamin.Extensions.ObjectMappers.Abstractions;
 
 namespace AYweb.Application.Models.User.Commands.CreateUser
 {
-    public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, bool>
+    public class CreateUserCommandHandler : ICommandHandler<CreateUserCommand, long>
     {
         private readonly IMapperAdapter _mapper;
         private readonly IUserRepository _repository;
@@ -19,14 +19,16 @@ namespace AYweb.Application.Models.User.Commands.CreateUser
             _sender = sender;
         }
 
-        public Task<bool> Handle(CreateUserCommand request, CancellationToken cancellationToken)
+        public Task<long> Handle(CreateUserCommand request, CancellationToken cancellationToken)
         {
             string hashPassword = PasswordHelper.EncodePasswordMd5(request.Password);
             request.Password = hashPassword;
 
-            _repository.Add(_mapper.Map<CreateUserCommand, Domain.Models.User.Entities.User>(request));
+            Domain.Models.User.Entities.User user = _mapper.Map<CreateUserCommand, Domain.Models.User.Entities.User>(request);
+            
+            _repository.Add(user);
             _repository.Save();
-            return Task.FromResult(true);
+            return Task.FromResult(user.Id);
         }
     }
 }
