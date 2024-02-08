e9a1a79aa0b639925993a223f66ca014b057abaf
yasin abedini <yasinabedini1384@gmail.com>
2024-02-08 13:41:21 +0330

    feat - Add Approve traction commnad

---
+++ src/3.Application/AYweb.Application/Models/User/Commands/ApprovePhoneNumber/ApprovePhoneNumberCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using AIPFramework.Commands;
+using AYweb.Domain.Models.User.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AYweb.Application.Models.User.Commands.ApprovePhoneNumber
+{
+    public class ApprovePhoneNumberCommandHandler : ICommandHandler<ApprovePhoneNumberCommand>
+    {
+        private readonly IUserRepository _repository;
+
+        public ApprovePhoneNumberCommandHandler(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task Handle(ApprovePhoneNumberCommand request, CancellationToken cancellationToken)
+        {
+            var user = _repository.GetUSerByPhoneNumber(request.PhoneNumber);
+            user.ConfirmPhoneNumber();
+
+            _repository.Update(user);
+            _repository.Save();
+
+            return Task.CompletedTask;
+        }
+    }
+}
