c0c8a7ee7b581a4eb528a060e4dd4c5dbfbbbff4
yasin abedini <yasinabedini1384@gmail.com>
2024-02-09 21:04:53 +0330

    feat - Add Command to transaction

--- src/3.Application/AYweb.Application/Models/Transaction/Commands/ApproveTransaction/ApproveTransactionCommandHandler.cs
+++ src/3.Application/AYweb.Application/Models/Transaction/Commands/ApproveTransaction/ApproveTransactionCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using AIPFramework.Commands;
+using AYweb.Domain.Models.Order.Repositories;
+using AYweb.Domain.Models.Transaction.Enums;
 using AYweb.Domain.Models.Transaction.Repositories;
 using System;
 using System.Collections.Generic;
@@ -10,18 +12,31 @@ namespace AYweb.Application.Models.Transaction.Commands.ApproveTransaction
 {
     public class ApproveTransactionCommandHandler : ICommandHandler<ApproveTransactionCommand>
     {
-
+        private readonly IOrderRepository _orderRepository;
         private readonly ITransactionRepository _repository;
 
-        public ApproveTransactionCommandHandler(ITransactionRepository repository)
+        public ApproveTransactionCommandHandler(ITransactionRepository repository, IOrderRepository orderRepository)
         {
             _repository = repository;
+            _orderRepository = orderRepository;
         }
 
         public Task Handle(ApproveTransactionCommand request, CancellationToken cancellationToken)
         {
             _repository.ApproveTransaction(request.Id);
             _repository.Save();
+
+
+            if (_repository.GetById(request.Id).Type.Value == _TransactionType.PaymentOrder.ToString())
+            {
+                var order = _orderRepository.GetList().SingleOrDefault(t => t.TransactionId == request.Id);
+                if (order is not null)
+                {
+                    order.ApproveOrder();                    
+                    _orderRepository.Update(order);
+                    _orderRepository.Save();
+                }
+            }
 
             return Task.CompletedTask;
         }
--- src/3.Application/AYweb.Application/Models/Transaction/Commands/RejectTransaction/RejectTransactionCommandHandler.cs
+++ src/3.Application/AYweb.Application/Models/Transaction/Commands/RejectTransaction/RejectTransactionCommandHandler.cs
@@ -1,4 +1,6 @@
 ﻿using AIPFramework.Commands;
+using AYweb.Domain.Models.Order.Repositories;
+using AYweb.Domain.Models.Transaction.Enums;
 using AYweb.Domain.Models.Transaction.Repositories;
 
 namespace AYweb.Application.Models.Transaction.Commands.RejectTransaction
@@ -7,14 +9,24 @@ namespace AYweb.Application.Models.Transaction.Commands.RejectTransaction
     {
 
         private readonly ITransactionRepository _repository;
+        private readonly IOrderRepository _orderRepository;
 
-        public RejectTransactionCommandHandler(ITransactionRepository repository)
+        public RejectTransactionCommandHandler(ITransactionRepository repository, IOrderRepository orderRepository)
         {
             _repository = repository;
+            _orderRepository = orderRepository;
         }
         public Task Handle(RejectTransactionCommand request, CancellationToken cancellationToken)
         {
             _repository.RejectTransaction(request.Id);
+
+            if (_repository.GetByIdWithRelations(request.Id).Type.Value == _TransactionType.PaymentOrder.ToString())
+            {
+                var order = _orderRepository.GetList().SingleOrDefault(t => t.TransactionId == request.Id);
+                order.RejectOrder();
+                _orderRepository.Update(order);
+            }
+
             _repository.Save();
 
             return Task.CompletedTask;
